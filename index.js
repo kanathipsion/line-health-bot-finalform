b91d4e95089d9ed33a85123c8d903e4356eb76c7
kanathipsion <bot.kanathip.sion@gmail.com>
2024-11-17 02:11:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -47,11 +47,6 @@ app.get('/form', (req, res) => {
   res.sendFile(path.join(__dirname, 'form.html'));
 });
 
-// เส้นทางหลัก (optional)
-app.get('/', (req, res) => {
-  res.send('LINE Bot Health Check Application');
-});
-
 // Start server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
