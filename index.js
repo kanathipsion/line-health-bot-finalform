a0edaebb041296ef9039915d149a647c52ec1eee
kanathipsion <bot.kanathip.sion@gmail.com>
2024-11-18 23:21:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -31,14 +31,16 @@ if (fbs >= 126 || bpSystolic >= 140 || bpDiastolic >= 90) {
       originalContentUrl: 'https://drive.google.com/uc?id=1Z9YF0VVLF8EVnKHDu9LxVnmAojAVZrd-',
       previewImageUrl: 'https://drive.google.com/uc?id=1Z9YF0VVLF8EVnKHDu9LxVnmAojAVZrd-'
     });
-} else if ((fbs >= 100 && fbs < 126) || (bpSystolic >= 120 && bpSystolic < 140) || (bpDiastolic >= 80 && bpDiastolic < 90)) {
+} 
+if ((fbs >= 100 && fbs < 126) || (bpSystolic >= 120 && bpSystolic < 140) || (bpDiastolic >= 80 && bpDiastolic < 90)) {
     // กลุ่มเสี่ยง (สีเหลือง)
     messages.push({
       type: 'image',
       originalContentUrl: 'https://drive.google.com/uc?id=1U41tRXROkj9v6lmHNKqAJ2vLyA3CUREi',
       previewImageUrl: 'https://drive.google.com/uc?id=1U41tRXROkj9v6lmHNKqAJ2vLyA3CUREi'
     });
-} else if (fbs < 100 && bpSystolic < 120 && bpDiastolic < 80) {
+} 
+if (fbs < 100 && bpSystolic < 120 && bpDiastolic < 80) {
     // กลุ่มปกติ (สีเขียว)
     messages.push({
       type: 'image',
@@ -46,6 +48,7 @@ if (fbs >= 126 || bpSystolic >= 140 || bpDiastolic >= 90) {
       previewImageUrl: 'https://drive.google.com/uc?id=1neLxgykGoVpyPMWaofsqgtmauVHRvj5s'
     });
 }
+
 
 
   const body = {
